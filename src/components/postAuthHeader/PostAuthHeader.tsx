2707bb2cc3f7aad4cb75d4376cf9d84d3ae3f30a
Atul Kumar Tiwari <atulforworkk@gmail.com>
2024-11-15 00:21:48 +0530

    new changes

--- src/components/postAuthHeader/PostAuthHeader.tsx
+++ src/components/postAuthHeader/PostAuthHeader.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
-    heading:string |null;
-}
+  heading: string | null;
+};
 
-const PostAuthHeader = ({heading}: Props) => {
+const PostAuthHeader = ({ heading }: Props) => {
+  const handleClcik=()=>{
+
+  }
   return (
     <div className="p-2 flex items-center shadow-xl">
-    <button className="bg-main-color p-2 w-8 h-8 rounded-full text-white flex items-center justify-center ">
-      &lt;
-    </button>
-    <h1 className="text-center flex-grow font-bold">{heading}</h1>
-  </div>
-  )
-}
+      <button className="bg-main-color p-2 w-8 h-8 rounded-full text-white flex items-center justify-center ">
+        &lt;
+      </button>
+      <h1 className="text-center flex-grow font-bold">{heading}</h1>
+      <button className="bg-main-color p-2 w-20 h-8 rounded-full text-white flex items-center justify-center hover:bg-custom-Purple" onClick={handleClcik}>
+        Logout
+      </button>
+    </div>
+  );
+};
 
-export default PostAuthHeader
+export default PostAuthHeader;
--- src/views/page/myWallet/MyWallet.tsx
+++ src/views/page/myWallet/MyWallet.tsx
@@ -1,5 +1,6 @@
 import PostAuthHeader from "@/components/postAuthHeader/PostAuthHeader";
 import TransactionView from "@/composites/transaction/TransactionView";
+import UserCard from "@/composites/userCard/UserCard";
 import { Button, Input, Radio } from "@mantine/core";
 import images from "images/images";
 import React from "react";
@@ -7,33 +8,12 @@ import { useNavigate } from "react-router-dom";
 type Props = {};
 
 const MyWallet = (props: Props) => {
-
   return (
     <div>
       <PostAuthHeader heading="My Wallet" />
       <div className="grid grid-cols-1 md:grid-cols-12 gap-4 p-2">
         {/* USER SECTION  */}
-        <div className="border round-md col-span-6 ">
-          <div>
-            <img
-              src={images.userImage}
-              alt="userImage"
-              className="w-[46px] h-[46px] p-[8px] pt-2 rounded-full "
-            />
-          </div>
-          <div className="w-10/12">
-            <div className="flex justify-between">
-              <h1 className="font-bold">Alex Smith </h1>
-              <div className="flex px-3 py-0.5 rounded-full bg-light-purple justify-around">
-                <img src={images.verfiedBadge} alt="" className="" />
-                <p className="mx-1">Verfied</p>
-              </div>
-            </div>
-            <div>
-              <p>+91 8930-188-923</p>
-            </div>
-          </div>
-        </div>
+        <UserCard />
         {/* available Credit */}
         <div className="rounded-md  col-span-6 bg-light-gray border">
           <h1 className="m-4 font-bold">Available Credits</h1>
@@ -76,9 +56,9 @@ export default MyWallet;
 
 const Card = () => {
   const navigate = useNavigate();
-  const handleClick =()=>{
-    navigate("/payment")
-  }
+  const handleClick = () => {
+    navigate("/payment");
+  };
   return (
     <div className="border rounded-2xl bg-light-gray border-black min-w-[350px]">
       <div className="flex justify-between items-center m-3 ">
